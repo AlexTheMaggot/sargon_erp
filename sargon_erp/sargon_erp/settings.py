7ccf0e952ba3d2fba5795a7f52daf19a1518c074
alexagapkin <alexthemaggot23@gmail.com>
2026-06-26 16:12:16 +0500

    - fix static and media settings

--- sargon_erp/sargon_erp/settings.py
+++ sargon_erp/sargon_erp/settings.py
@@ -93,9 +93,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = BASE_DIR / 'mainapp/static'
-MEDIA_ROOT = BASE_DIR / 'mainapp/media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'mainapp/static',
 ]
