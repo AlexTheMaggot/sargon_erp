668cd8517de7ac1732c4d40a6d6fce1aae582345
alexagapkin <alexthemaggot23@gmail.com>
2026-07-07 15:02:05 +0500

    - add base urls

--- sargon_erp/sargon_erp/urls.py
+++ sargon_erp/sargon_erp/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
