2bda49dcc90d8435839fa42171296057f4631bec
alexagapkin <alexthemaggot23@gmail.com>
2026-07-14 12:44:00 +0500

    - add start path

--- frontend/src/constants/forms.js
+++ frontend/src/constants/forms.js
@@ -1,10 +1,10 @@
 export const emptyUserForm = { id: null, username: '', password: '', first_name: '', last_name: '', email: '', group_id: '', is_active: true }
-export const emptyGroupForm = { id: null, name: '', description: '', module_ids: [] }
-export const emptyModuleForm = { id: null, code: '', name: '', description: '', is_active: true }
+export const emptyGroupForm = { id: null, name: '', description: '', module_ids: [], start_module_id: '' }
+export const emptyModuleForm = { id: null, code: '', name: '', description: '', default_path: '', is_active: true }
 export const emptyPermissionForm = { id: null, code: '', name: '', description: '' }
 export const emptySupplierForm = { id: null, company_name: '', city_id: '', address: '', phone: '' }
 export const emptyCityForm = { id: null, name: '' }
-export const emptyReceiptForm = { id: null, supplier_id: '', input_quantity: '', input_unit: 'liters', can_set_quantity: false, can_complete: false, is_locked: false, is_existing: false }
+export const emptyReceiptForm = { id: null, supplier_id: '', input_quantity: '', input_unit: 'liters', can_set_quantity: false, can_complete: false, is_completed: false, is_locked: false, is_existing: false, batches: [] }
 export const emptyLaboratoryAnalysisForm = {
   id: null,
   density: '',
